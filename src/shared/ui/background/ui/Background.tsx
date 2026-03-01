b88c0b1a9931366d1f27fb5218d716d813894bf1
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-03-01 14:40:27 +0300

    added three.js to deps

--- src/shared/ui/background/ui/Background.tsx
+++ src/shared/ui/background/ui/Background.tsx
@@ -1,13 +1,17 @@
+import React from "react";
+
 type BackgroundProps = {
     videoSrc: string;
     fixed?: boolean;
+    videoRef?: React.Ref<HTMLVideoElement | null>
 }
 
-export function Background({videoSrc, fixed}: BackgroundProps) {
+export function Background({videoSrc, fixed, videoRef}: BackgroundProps) {
     return (
         <div className={`${fixed ? 'fixed' : 'absolute'} -z-10 inset-0`}>
             <video
                 src={videoSrc}
+                ref={videoRef}
                 className='absolute inset-0 h-full w-full object-cover'
                 autoPlay
                 loop
