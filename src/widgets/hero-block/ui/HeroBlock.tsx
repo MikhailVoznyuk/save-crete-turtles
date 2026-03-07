6c3e50611a55d418a6acc74154fe5a050633f47c
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-03-07 23:58:22 +0300

    added Bubbles import

--- src/widgets/hero-block/ui/HeroBlock.tsx
+++ src/widgets/hero-block/ui/HeroBlock.tsx
@@ -6,6 +6,7 @@ import {TextBlock} from "@/shared/ui/text-blocks";
 import {WaveButton} from "@/shared/ui/buttons/wave-button";
 import {JellyContainer} from "@/shared/ui/containers";
 import {LiquidGlass} from "@/shared/effects/liquid-glass";
+import {Bubbles} from "@/widgets/hero-block/ui/Bubbles";
 
 export function HeroBlock() {
     return (
@@ -25,66 +26,9 @@ export function HeroBlock() {
 
                 </div>
             </div>
-            <div className='absolute right-32 top-64 md'>
-                <JellyContainer
-                    className="rounded-full"
-                    outlineClassName="stroke-cyan-200/20"
-                    innerClassName="p-20"
-                    outline
-                    boundaryPoints={72}
-                    bendK={200}
-                    smoothK={0.44}
-                    smoothIters={2}
-                    pathTension={0.6}
-                    pressureK={20000}
-                    shapeK={85}
-                    damping={7.2}
-                    hoverIndent={2.0}
-                    hoverRadius={140}
-                    hoverIndentMul={1.35}
-                    hoverEnterMul={1.25}
-                    hoverIndentSigmaFactor={0.30}
-                    hoverEnterSigmaFactor={0.30}
-                    hoverIndentWeightPow={1.55}
-                    hoverEnterWeightPow={1.25}
-                    hoverPressureBoost={0.0}
-                    hoverRingStrength={720}
-                    hoverRingMul={1.15}
-                    hoverRingLengthFactor={0}
-                    hoverConeMul={1.2}
-                    hoverConeWidthBaseFactor={0.26}
-                    hoverConeWidthSlopeFactor={0.46}
-                    hoverConeLengthFactor={2.3}
-                    hoverConeNormalSpeedGain={1200}
-                    clickIndent={0.01}
-                    clickWave={0.9}
-                    pointerSpeedMax={10000}
-                    hoverFastBoost={10}
-                    idle
-                    idleStrength={1000}
-                    idleFreq={0.28}
-                    idleWaves={1.3}
-                    idleTurbulence={0.7}
-                    idleTangential={0.20}
-                    idleInteractMul={0.15}
 
-                >
-                    <LiquidGlass
-                        intensity={1.15}
-                        magnify={0.12}
-                        blur={0.42}
-                        chromatic={0.18}
-                        rim={0.55}
-                        spec={0.55}
-                        tint={0.35}
-                        alpha={1}
-                        dirMode={0}
-                    />
-                    <div>
-                        <TextBlock size="xl">Help Me to survive!</TextBlock>
-                    </div>
-                </JellyContainer>
-            </div>
+                <Bubbles />
+
 
 
 
