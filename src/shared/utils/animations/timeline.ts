3cd30f94b018ee99fe1bd1179e41cda9ceb33a89
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-03-09 23:57:36 +0300

    update: remove recursion in run, added clear method and remove private property of applyStyles

--- src/shared/utils/animations/timeline.ts
+++ src/shared/utils/animations/timeline.ts
@@ -1,13 +1,12 @@
-import React from "react";
-
 type Style = Record<string, string>;
 
 type InitProps = {
     el: HTMLElement;
     initStyle?: Style;
+    loop?: boolean;
 }
 
-type AddProps = {
+type StepProps = {
     style: Style;
     duration?: number;
     delay?: number;
@@ -16,35 +15,75 @@ type AddProps = {
 
 export class Timeline {
     el: HTMLElement;
+    stepsQueue: StepProps[];
     timeline: Promise<void>;
+    stepInd: number;
+    running: boolean;
+    loop: boolean;
+    initStyle?: Style;
 
-   constructor({el, initStyle}: InitProps) {
+
+   constructor({el, initStyle, loop=false}: InitProps) {
        this.el = el;
-
+       this.stepInd = 0;
+       this.stepsQueue = [];
+       this.running = false;
+       this.loop = loop;
        if (initStyle) {
-           this.applyStyles({'transition': 'none'});
-           this.applyStyles(initStyle);
+           this.initStyle = initStyle;
+           this.applyStyles(this.initStyle);
        }
 
        this.timeline = Promise.resolve();
    }
 
-   add({style, duration=300, delay=0, stay=1000}: AddProps) {
-       const step = (resolve: () => void,) => {
-           this.el.style.transition = `${duration}ms ease`;
-           setTimeout(() => {
-               this.applyStyles(style);
-               setTimeout(() => {
-                   resolve();
-               }, duration + stay);
-           }, delay);
-       }
+   async run() {
+       if (this.running || this.stepsQueue.length === 0) return;
 
-       this.timeline = this.timeline.then(() => new Promise(step));
+       while (true) {
+
+           if (this.stepInd >= this.stepsQueue.length) {
+               if (this.loop) {
+                   if (this.initStyle) this.applyStyles(this.initStyle);
+                   this.stepInd = 0;
+               } else {
+                   console.log('done')
+                   break;
+               }
+           } else {
+
+
+               await new Promise<void>(resolve => {
+                   setTimeout(() => {
+                       console.log('we workin');
+                       const step = this.stepsQueue[this.stepInd];
+                       if (!step) return resolve();
+                       this.el.style.transition = `${step.duration}ms ease`;
+                       this.applyStyles(step.style);
+                       setTimeout(() => resolve(), (step.duration ?? 0) + (step.stay ?? 0));
+                   }, this.stepsQueue[this.stepInd].delay ?? 0)
+               });
+
+               this.stepInd++;
+           }
+       }
+   }
+
+   add(step: StepProps) {
+       this.stepsQueue.push(step);
+       this.run().catch(err => console.log(err));
+       console.log('added', this.stepsQueue)
        return this;
    }
 
-   private applyStyles(style: Style): void {
+   clear(reload: boolean = true) {
+       this.stepsQueue = [];
+       if (reload) {
+           this.stepInd = 0;
+       }
+   }
+
+   applyStyles(style: Style): void {
        for (const key in style) {
            const value = this.toKebabCase(style[key]);
            this.el.style.setProperty(key, value);
