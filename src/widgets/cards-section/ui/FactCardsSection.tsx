aab0c1dac546abf270aff94185a8da36f3034869
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-03-15 23:50:12 +0300

    update: added id prop to FactCard and boiler templates to CARD_DATA

--- src/widgets/cards-section/ui/FactCardsSection.tsx
+++ src/widgets/cards-section/ui/FactCardsSection.tsx
@@ -4,6 +4,19 @@ import type {FactCardProps} from "@/widgets/cards-section/ui/FactCard";
 
 const CARD_DATA: FactCardProps[] = [
     {
+        id: 'survival',
+        title: <>Only <span className='text-turk'>1 in 1000</span> survives.</>,
+        icon:'/media/icons/cards/turtle.png',
+        description: 'Since sea turtles grow very slowly, they can take up to 50 years to reach sexual maturity–which is quite late in life! Sadly, they also have a very low natural survival rate: it is estimated that only one in 1,000 sea turtle hatchlings survive to adulthood.'
+    },
+    {
+        id: 'plastic',
+        title: <>Only <span className='text-turk'>1 in 1000</span> survives.</>,
+        icon:'/media/icons/cards/turtle.png',
+        description: 'Since sea turtles grow very slowly, they can take up to 50 years to reach sexual maturity–which is quite late in life! Sadly, they also have a very low natural survival rate: it is estimated that only one in 1,000 sea turtle hatchlings survive to adulthood.'
+    },
+    {
+        id: 'hunters',
         title: <>Only <span className='text-turk'>1 in 1000</span> survives.</>,
         icon:'/media/icons/cards/turtle.png',
         description: 'Since sea turtles grow very slowly, they can take up to 50 years to reach sexual maturity–which is quite late in life! Sadly, they also have a very low natural survival rate: it is estimated that only one in 1,000 sea turtle hatchlings survive to adulthood.'
@@ -16,8 +29,8 @@ export function FactCardsSection() {
             <Title variant='primary' size='lg' lined centered>
                 Why it is important?
             </Title>
-            <div className='flex gap-4'>
-                {CARD_DATA.map((card: FactCardProps) => <FactCard {...card} />)}
+            <div className='flex gap-8'>
+                {CARD_DATA.map((card: FactCardProps) => <FactCard key={card.id} {...card} />)}
             </div>
         </section>
     )
