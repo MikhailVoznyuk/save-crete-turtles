551d0da2c404299138dec40ad162695da0b4adaa
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-03-19 02:08:39 +0300

    update: changed button styles

--- src/shared/ui/buttons/modal-toggle-button/ModalToggleButton.tsx
+++ src/shared/ui/buttons/modal-toggle-button/ModalToggleButton.tsx
@@ -7,10 +7,10 @@ type ModalToggleButtonProps = {
     className?: string;
 }
 
-const base = 'flex items-center justify-center rounded-full border backdrop-blur-xs transition-all duration-300';
-const size = 'size-16'
-const tone = 'bg-cold-white/20 border-white/50 ';
-const hover = 'hover:bg-turk hover:border-turk'
+const base = 'rounded-full border overflow-hidden backdrop-blur-xs cursor-pointer transition-all duration-300 group/fs-btn';
+const size = 'size-12'
+const tone = 'bg-black/10 border-white/50';
+const hover = 'hover:border-turk'
 
 export function ModalToggleButton({variant='primary', onClick, className}: ModalToggleButtonProps) {
 
@@ -19,18 +19,21 @@ export function ModalToggleButton({variant='primary', onClick, className}: Modal
             onClick={onClick}
             className={twMerge(base, size, tone, hover, className)}
         >
-            <Image src='/media/icons/fullscreen_passive.svg'
-                   className='absolute top-1/2 left-1/2 -translate-1/2 size-12 hover:opacity-0 transition-300 duration-300'
-                   width={46}
-                   height={46}
-                   alt='open modal icon'
-            />
-            <Image src='/media/icons/fullscreen_active.svg'
-                   className='absolute top-1/2 left-1/2 -translate-1/2 size-12 opacity-0 hover:opacity-1 transition-300 duration-300'
-                   width={46}
-                   height={46}
-                   alt='open modal icon'
-            />
+            <div className='relative size-full flex items-center justify-center'>
+                <Image src='/media/icons/fullscreen_passive.svg'
+                       className='absolute top-1/2 left-1/2 -translate-1/2 size-9 group-hover/fs-btn:opacity-0 transition-300 duration-300'
+                       width={36}
+                       height={36}
+                       alt='open modal icon'
+                />
+                <Image src='/media/icons/fullscreen_active.svg'
+                       className='absolute top-1/2 left-1/2 -translate-1/2 size-9 opacity-0 group-hover/fs-btn:opacity-100 transition-300 duration-300'
+                       width={36}
+                       height={36}
+                       alt='open modal icon'
+                />
+            </div>
+
         </button>
     )
 }
