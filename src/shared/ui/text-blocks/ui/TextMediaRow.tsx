a02ebad2d03503dd755ac9aeb3ebcf16f7a1c0d5
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-03-19 22:28:14 +0300

    update: changed layout

--- src/shared/ui/text-blocks/ui/TextMediaRow.tsx
+++ src/shared/ui/text-blocks/ui/TextMediaRow.tsx
@@ -5,12 +5,21 @@ import type {TextMediaRowProps} from "@/shared/ui/media/types/types";
 
 export function TextMediaRow({header, content, mediaType, mediaSrc, reversed=false}: TextMediaRowProps)  {
     return (
-        <div className={`flex gap-10 items-center justify-center ${(reversed) ? 'flex-row-reverse' : 'flex-row'}`}>
-            <div className='flex flex-col gap-4 w-1/2 max-w-[500px]'>
-                <Title variant='secondary' size='md' lined titleClassName='text-turk' lineClassName='bg-cloud'>{header}</Title>
-                <TextBlock size='md'>{content}</TextBlock>
+        <div className={`flex flex-col gap-8 items-center justify-center `}>
+            <div className={`w-full flex ${reversed ? 'justify-end' : 'justify-start'}`}>
+                <Title
+                    variant='secondary'
+                    size='lg'
+                    lined
+                    titleClassName='text-turk'
+                    lineClassName={`bg-cloud w-3/4`}
+                    containerClassName={reversed ? 'items-end' : 'items-start'}
+                >{header}</Title>
             </div>
-            <ModalImage src={mediaSrc} className='w-1/2 max-h-96' />
+            <div className={`flex gap-14 ${(reversed) ? 'flex-row-reverse' : 'flex-row'}`}>
+                <TextBlock className='max-w-1/2' size='md'>{content}</TextBlock>
+                <ModalImage src={mediaSrc} className='w-1/2 max-h-96' />
+            </div>
         </div>
     )
 }
