e4bf40696b1be396569380d4fc15ea61efcb9ce6
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-03-19 22:27:32 +0300

    update: added btnNeeded prop, added MediaModal wrapper

--- src/shared/ui/media/ui/ModalImage.tsx
+++ src/shared/ui/media/ui/ModalImage.tsx
@@ -1,33 +1,57 @@
+'use client'
+
 import Image from "next/image";
+import {motion} from "framer-motion";
 import {twMerge} from "tailwind-merge";
 import {ModalToggleButton} from "@/shared/ui/buttons/modal-toggle-button";
+import {MediaModal} from "@/shared/ui/media-modal";
 
 type ModalImageProps = {
     src: string;
     width?: number;
     height?: number;
+    btnNeeded?: boolean;
     className?: string;
     imageClassName?: string;
     alt?: string;
 }
 
-export function ModalImage({src, className, imageClassName, alt, width=1920, height=1080}: ModalImageProps) {
+export function ModalImage({src, className, imageClassName, alt, width=1920, height=1080, btnNeeded=true}: ModalImageProps) {
+
     return (
-        <div className={twMerge(
-            'relative rounded-2xl shadow-xl border cursor-pointer border-cold-white/50 overflow-hidden',
-                className
-        )}>
-            <Image 
-                className={twMerge('size-full object-cover', imageClassName)}
-                width={width}
-                height={height}
-                src={src}
-                alt={alt ?? 'Side photo of text block'}
-            />
-            <ModalToggleButton
-                onClick={() => console.log('clicked')}
-                className={twMerge('absolute bottom-4 right-4')}
-            />
-        </div>
+        <MediaModal
+            preview={({open, layoutId}) => (
+                <motion.div
+                    layoutId={layoutId}
+                    className={twMerge(
+                    'relative rounded-2xl shadow-xl border-2 cursor-pointer border-cold-white/50 hover:border-turk/80 overflow-hidden',
+                    className
+                )}>
+                    <img
+                        onClick={open}
+                        className={twMerge('size-full object-cover', imageClassName)}
+                        width={width}
+                        height={height}
+                        src={src}
+                        alt={alt ?? 'Side photo of text block'}
+                    />
+                    {btnNeeded && (
+                        <ModalToggleButton
+                            onClick={open}
+                            className={twMerge('absolute bottom-4 right-4')}
+                        />
+                    )}
+                </motion.div>
+            )}
+            content={() => (
+                <Image
+                    className='w-full max-h-[85vh] object-cover'
+                    src={src}
+                    alt={alt ?? 'fullscreen side photo of text block'}
+                    width={width}
+                    height={height}
+                />
+            )}
+        />
     )
 }
