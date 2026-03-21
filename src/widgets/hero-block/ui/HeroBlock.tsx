4697afc2b4da990d756ec0064f8410c9b7596c37
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-03-21 23:57:48 +0300

    update: added adaptive for mobile devices

--- src/widgets/hero-block/ui/HeroBlock.tsx
+++ src/widgets/hero-block/ui/HeroBlock.tsx
@@ -1,26 +1,29 @@
-'use client'
+'use client';
 
+import {useEffect, useState, useRef} from "react";
 import {Title} from "@/shared/ui/text-blocks/ui/Title";
-import {Background} from "@/shared/ui/background";
 import {TextBlock} from "@/shared/ui/text-blocks";
 import {WaveButton} from "@/shared/ui/buttons/wave-button";
-import {JellyContainer} from "@/shared/ui/containers";
-import {LiquidGlass} from "@/shared/effects/liquid-glass";
 import {Bubbles} from "@/widgets/hero-block/ui/Bubbles";
 import {ArrowButton} from "@/shared/ui/buttons/arrow-button";
+import {useIsMobile} from "@/shared/hooks/adaptive";
 
 export function HeroBlock() {
+    const isMobile = useIsMobile();
+    const rafRef = useRef<number | null>(null);
+
+
     return (
         <section className='relative w-full h-screen'>
-            <div className='flex size-full p-10 text-9'>
-                <div className='flex flex-col gap-4 pt-[20vh] max-w-[740px]'>
-                    <Title as={'h1'} size={'xl'} lined>
+            <div className='flex flex-col size-full p-4 pb-32 justify-end sm:justify-start sm:p-10 text-9'>
+                <div className='flex flex-col gap-4 pt-[20vh] max-w-[740px] items-center sm:items-start'>
+                    <Title as={'h1'} size={'xl'} lined centered={isMobile}>
                         Help us save the Cretan turtles in Almyrida
                     </Title>
-                    <TextBlock className='w-11/12' size={'xl'} uppercase>
+                    <TextBlock className='w-11/12' size={'xl'} uppercase centered={isMobile}>
                         {"Contribute to improving the turtles' living conditions and survival. It's not difficult, but it will help save their lives."}
                     </TextBlock>
-                    <div className='flex gap-4 items-center'>
+                    <div className='flex flex-col sm:flex-row gap-4 items-center sm:items-center '>
                         <WaveButton onClick={() => {}}>Read More</WaveButton>
                         <WaveButton onClick={() => {}} variant='secondary'>Contact us</WaveButton>
                     </div>
