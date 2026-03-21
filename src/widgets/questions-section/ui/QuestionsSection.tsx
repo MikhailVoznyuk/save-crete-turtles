2e02ccb2691ff9d44d796a3ddb7e94a08bad3585
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-03-21 20:13:27 +0300

    update: add cards content

--- src/widgets/questions-section/ui/QuestionsSection.tsx
+++ src/widgets/questions-section/ui/QuestionsSection.tsx
@@ -14,7 +14,7 @@ const QaABlocks: QaABlock[] = [
         id: 'qa-1',
         header: 'Why is the main eney of turtles',
         content: 'The main enemy of sea turtles are plastic bags, which they mistake for jellyfish and swallow them. Unfortunately, the turtle\'s intestines cannot digest a plastic bag. It gets stuck, clogs the intestines, and can only be removed by surgery, otherwise the turtle dies. Therefore, we created this site and kindly ask everyone not to throw bags on the beach because the wind blows them out to sea. If you see plastic bags in the water, as did those in our habitat in Souda Bay, Chania, Crete: please take plastic garbage out of the water and throw it in the trash. Think about it: by doing so, you may have saved one or maybe two turtles, and then these sea turtles will give birth to offspring.',
-        iconSrc: '/media/icons/cards/turtle.png'
+        iconSrc: '/media/icons/cards/plastic.png'
     },
     {
         id: 'qa-2',
