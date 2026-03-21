efee9453fe423cd73d4efacf8f57666edbb98d5d
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-03-22 00:46:41 +0300

    update:  added objectPos prop

--- src/shared/ui/background/ui/Background.tsx
+++ src/shared/ui/background/ui/Background.tsx
@@ -3,16 +3,21 @@ import React from "react";
 type BackgroundProps = {
     videoSrc: string;
     fixed?: boolean;
-    videoRef?: React.Ref<HTMLVideoElement | null>
+    videoRef?: React.Ref<HTMLVideoElement | null>;
+    objectPos?: {x: number; y: number} | null;
+    videoSize?: {w: number; h: number};
 }
 
-export function Background({videoSrc, fixed, videoRef}: BackgroundProps) {
+export function Background({videoSrc, fixed, videoRef, objectPos, videoSize={w: 1920, h: 1080}}: BackgroundProps) {
     return (
         <div className={`${fixed ? 'fixed' : 'absolute'} -z-10 inset-0`}>
             <video
                 src={videoSrc}
                 ref={videoRef}
                 className='absolute inset-0 h-full w-full object-cover'
+                style={objectPos ? {
+                    objectPosition: `${objectPos.x / videoSize?.w * 100}% ${objectPos.y / videoSize?.h * 100}%`
+                } : {}}
                 autoPlay
                 loop
                 muted
