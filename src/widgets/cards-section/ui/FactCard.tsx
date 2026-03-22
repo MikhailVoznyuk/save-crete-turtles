7859d996629985cf116eb850a0ef6090bbdd7765
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-03-22 17:42:32 +0300

    update:  mobile adaptive for Hero section

--- src/widgets/cards-section/ui/FactCard.tsx
+++ src/widgets/cards-section/ui/FactCard.tsx
@@ -4,6 +4,7 @@ import {GlassBubble} from "@/shared/ui/containers/glass-bubble";
 import {Title} from "@/shared/ui/text-blocks/ui/Title";
 import {TextBlock} from "@/shared/ui/text-blocks";
 import {ArrowButton} from "@/shared/ui/buttons/arrow-button";
+import {useIsMobile} from "@/shared/hooks/adaptive";
 import React from "react";
 
 export type FactCardProps = {
@@ -18,15 +19,15 @@ export type FactCardProps = {
 export function FactCard({ icon, title, description, iconAlt }: FactCardProps) {
     const [opened, setOpened] = useState(false);
     const [contentHeight, setContentHeight] = useState<number>(0);
-
+    const isMobile = useIsMobile();
 
     const setHeight = (el: HTMLDivElement | null) => {
         if (!el) return;
         setContentHeight(el.scrollHeight);
     }
 
-    const visibleHeight = 422;
-
+    const visibleHeight = (isMobile) ? 390 : 422;
+    const iconSize = (isMobile ? 42 : 50);
     const needDropDown = contentHeight > visibleHeight;
     console.log(needDropDown);
 
@@ -34,17 +35,17 @@ export function FactCard({ icon, title, description, iconAlt }: FactCardProps) {
         <GlassBubble
             effectStrength={'xs'}
             className='rounded-4xl'
-            innerClassName='p-5 flex items-center flex-col gap-4'
+            innerClassName='p-4 sm:p-5 flex items-center flex-col gap-4'
         >
             <div className='overflow-hidden'
                  style={{height: (opened) ? 'auto' : `${visibleHeight - ((needDropDown) ? 72 : 0)}px`}}
             >
                 <div ref={setHeight}
-                     className={`flex flex-col gap-4 w-72 items-center`}
+                     className={`flex flex-col gap-4 w-64 sm:w-72 items-center`}
                 >
                     <div className='flex flex-col items-center gap-4 w-full'>
-                        <div className='size-20 rounded-full bg-white/10 border border-cold-white/40 flex items-center justify-center shrink-0'>
-                            <Image src={icon} width={50} height={50} alt={iconAlt ?? 'icon of fact about turtles'} />
+                        <div className='size-[72px] sm:size-20 rounded-full bg-white/10 border border-cold-white/40 flex items-center justify-center shrink-0 shadow-xl sm:shadow-2xl'>
+                            <Image src={icon} width={iconSize} height={iconSize} alt={iconAlt ?? 'icon of fact about turtles'} />
                         </div>
                         <Title
                             size={'md'}
@@ -52,6 +53,7 @@ export function FactCard({ icon, title, description, iconAlt }: FactCardProps) {
                             lined
                             centered
                             containerClassName='shrink-0'
+                            titleClassName='font-medium'
                         >{title}</Title>
                     </div>
                     <TextBlock
